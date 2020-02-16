8e180808d107fa6e11d8ecce4dd3ce422f16c333
CWonLee <46565404+CWonLee@users.noreply.github.com>
2020-02-16 20:16:55 +0900

    메인화면 하단네비게이션바 구현

--- app/src/main/java/com/softsquared/daangnmarket/src/main/MainActivity.java
+++ app/src/main/java/com/softsquared/daangnmarket/src/main/MainActivity.java
@@ -1,26 +1,75 @@
 package com.softsquared.daangnmarket.src.main;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.softsquared.daangnmarket.R;
 import com.softsquared.daangnmarket.src.BaseActivity;
+import com.softsquared.daangnmarket.src.main.bottommenu.category.CategoryFragment;
+import com.softsquared.daangnmarket.src.main.bottommenu.chat.ChatFragment;
+import com.softsquared.daangnmarket.src.main.bottommenu.home.HomeFragment;
+import com.softsquared.daangnmarket.src.main.bottommenu.my.MyFragment;
+import com.softsquared.daangnmarket.src.main.bottommenu.write.WriteFragment;
 import com.softsquared.daangnmarket.src.main.interfaces.MainActivityView;
 
 public class MainActivity extends BaseActivity implements MainActivityView {
 
-    private TextView mTvHelloWorld;
+    private FragmentManager mFragmentManager = getSupportFragmentManager();
+    private HomeFragment mHomeFragment = new HomeFragment();
+    private CategoryFragment mCategoryFragment = new CategoryFragment();
+    private WriteFragment mWriteFragment = new WriteFragment();
+    private ChatFragment mChatFragment = new ChatFragment();
+    private MyFragment mMyFragment = new MyFragment();
+    BottomNavigationView mBottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTvHelloWorld = findViewById(R.id.main_tv_hello_world);
+        FragmentTransaction transaction = mFragmentManager.beginTransaction();
+        transaction.replace(R.id.main_frame_layout, mHomeFragment).commitAllowingStateLoss();
+
+        mBottomNavigationView = findViewById(R.id.main_btm_nav);
+        mBottomNavigationView.setItemIconTintList(null);
+        mBottomNavigationView.setOnNavigationItemSelectedListener(new ItemSelectedListener());
+    }
+
+    class ItemSelectedListener implements BottomNavigationView.OnNavigationItemSelectedListener{
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            FragmentTransaction transaction = mFragmentManager.beginTransaction();
+
+            switch(menuItem.getItemId())
+            {
+                case R.id.menu_item1:
+                    transaction.replace(R.id.main_frame_layout, mHomeFragment).commitAllowingStateLoss();
+                    break;
+                case R.id.menu_item2:
+                    transaction.replace(R.id.main_frame_layout, mCategoryFragment).commitAllowingStateLoss();
+                    break;
+                case R.id.menu_item3:
+                    transaction.replace(R.id.main_frame_layout, mWriteFragment).commitAllowingStateLoss();
+                    break;
+                case R.id.menu_item4:
+                    transaction.replace(R.id.main_frame_layout, mChatFragment).commitAllowingStateLoss();
+                    break;
+                case R.id.menu_item5:
+                    transaction.replace(R.id.main_frame_layout, mMyFragment).commitAllowingStateLoss();
+                    break;
+            }
+            return true;
+        }
     }
 
     private void tryGetTest() {
@@ -33,7 +82,6 @@ public class MainActivity extends BaseActivity implements MainActivityView {
     @Override
     public void validateSuccess(String text) {
         hideProgressDialog();
-        mTvHelloWorld.setText(text);
     }
 
     @Override
---
+++ app/src/main/java/com/softsquared/daangnmarket/src/main/bottommenu/write/WriteFragment.java
@@ -0,0 +1,21 @@
+package com.softsquared.daangnmarket.src.main.bottommenu.write;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.softsquared.daangnmarket.R;
+
+public class WriteFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_write, container, false);
+    }
+}
