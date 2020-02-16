1ad15e4ca51d1b63388c73829fe807852c0904c3
CWonLee <46565404+CWonLee@users.noreply.github.com>
2020-02-16 14:54:46 +0900

    splash

---
+++ app/src/main/java/com/softsquared/daangnmarket/src/splash/SplashActivity.java
@@ -0,0 +1,33 @@
+package com.softsquared.daangnmarket.src.splash;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.softsquared.daangnmarket.R;
+import com.softsquared.daangnmarket.src.BaseActivity;
+import com.softsquared.daangnmarket.src.main.MainActivity;
+
+public class SplashActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Handler hd = new Handler();
+        hd.postDelayed(new splashHandler(), 1000);
+    }
+
+    private class splashHandler implements Runnable{
+        public void run(){
+            startActivity(new Intent(getApplication(), MainActivity.class));
+            SplashActivity.this.finish();
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        //초반 플래시 화면에서 넘어갈때 뒤로가기 버튼 못누르게 함
+    }
+}
